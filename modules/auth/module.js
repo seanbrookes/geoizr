18b2953986587043026e6aba116ace10ad8e5c0c
Sean Brookes <sbrookes@beachair.ca>
2012-12-05 22:24:19 -0800

    initial commit

---
+++ modules/auth/module.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sean
+ * Date: 23/10/12
+ * Time: 11:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+LoginView= Backbone.View.extend({
+    tagName : "div",
+    className: "login",
+    render : function() {
+        // code for rendering the HTML for the view
+    }
+});
